6e527b9b0fb37b8fd974a64a2532f70a35b5deab
Stephen Lautier <stephen.lautier@outlook.com>
2017-12-09 20:10:50 +0100

    projects: update projects

--- src/app/areas/projects/projects.component.ts
+++ src/app/areas/projects/projects.component.ts
@@ -11,14 +11,14 @@ export class ProjectsComponent implements OnInit {
 		// js
 		{
 			key: "ssv-core",
-			title: "ssv-core",
+			title: "@ssv/core",
 			tag: "javascript",
-			url: "https://github.com/sketch7/ssv-ng2-command",
+			url: "https://github.com/sketch7/ssv-core",
 			description: "typescript core functions and utils",
 		},
 		{
 			key: "signalr-client",
-			title: "signalr client",
+			title: "@ssv/signalr-client",
 			tag: "javascript",
 			url: "https://github.com/sketch7/signalr-client",
 			description: "SignalR client library built on top of @aspnet/signalr-client. This gives you more features and easier to use.",
@@ -27,15 +27,22 @@ export class ProjectsComponent implements OnInit {
 		// ngx
 		{
 			key: "ng2-command",
-			title: "ng2 command",
+			title: "@ssv/ng2-command",
 			tag: "angular",
 			url: "https://github.com/sketch7/ssv-ng2-command",
 			description:
 				"Command pattern implementation for angular 2. Command's are used to encapsulate information which is needed to perform an action.",
 		},
 		{
+			key: "angular-skeleton-app",
+			title: "angular skeleton app",
+			tag: "angular",
+			url: "https://github.com/sketch7/angular-skeleton-app",
+			description: "Angular 5.x navigation skeleton project with styling which get you started faster.",
+		},
+		{
 			key: "ng2-heroes",
-			title: "ng2 heroes",
+			title: "ng2 heroes app",
 			tag: "angular",
 			url: "https://github.com/sketch7/ng2-heroes",
 			description: "angular2 heroes sample application",
@@ -44,14 +51,14 @@ export class ProjectsComponent implements OnInit {
 		// au
 		{
 			key: "ssv-au-core",
-			title: "ssv-au-core",
+			title: "@ssv/au-core",
 			tag: "aurelia",
 			url: "https://github.com/sketch7/ssv-au-core",
 			description: "Core components, utilities and services for Aurelia by Sketch7",
 		},
 		{
 			key: "ssv-au-ui",
-			title: "ssv-au-ui",
+			title: "@ssv/au-ui",
 			tag: "aurelia",
 			url: "https://github.com/sketch7/ssv-au-ui",
 			description: "UI components library for Aurelia by Sketch7",
