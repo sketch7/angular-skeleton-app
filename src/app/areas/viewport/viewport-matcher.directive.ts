85ddd8dd4c79738c354141b0b75655d89a892602
Stephen Lautier <stephen.lautier@outlook.com>
2018-10-13 22:03:12 +0200

    feat(viewport): giving it flavor ;) - only, exclude - single+multi

--- src/app/areas/viewport/viewport-matcher.directive.ts
+++ src/app/areas/viewport/viewport-matcher.directive.ts
@@ -1,87 +1,128 @@
+import * as _ from "lodash";
 import {
 	OnInit,
 	OnDestroy,
 	Directive,
 	Renderer2,
-	ElementRef,
 	ViewContainerRef,
 	Input,
 	EmbeddedViewRef,
 	TemplateRef,
 } from "@angular/core";
-import { Subscription } from "rxjs";
-import { tap, filter, delay } from "rxjs/operators";
+import { Subscription, Subject } from "rxjs";
+import { tap, filter, pairwise, startWith } from "rxjs/operators";
 
 import { ViewportService, ViewportSizeTypeInfo } from "./viewport.service";
-import { NgIfContext } from "@angular/common";
 
 @Directive({
 	selector: "[ssvViewportMatcher]",
 })
 export class SsvViewportMatcher implements OnInit, OnDestroy {
-	private _context: NgIfContext = new NgIfContext();
-	private _thenTemplateRef: TemplateRef<NgIfContext> | null = null;
-	private _elseTemplateRef: TemplateRef<NgIfContext> | null = null;
-	private _thenViewRef: EmbeddedViewRef<NgIfContext> | null = null;
-	private _elseViewRef: EmbeddedViewRef<NgIfContext> | null = null;
+	private _context: SsvViewportMatcherContext = new SsvViewportMatcherContext();
+	private _thenTemplateRef: TemplateRef<
+		SsvViewportMatcherContext
+	> | null = null;
+	private _elseTemplateRef: TemplateRef<
+		SsvViewportMatcherContext
+	> | null = null;
+	private _thenViewRef: EmbeddedViewRef<
+		SsvViewportMatcherContext
+	> | null = null;
+	private _elseViewRef: EmbeddedViewRef<
+		SsvViewportMatcherContext
+	> | null = null;
 	private sizeType$$: Subscription | undefined;
+	private update$$: Subscription | undefined;
+	private cssClass$$: Subscription | undefined;
+	private update$ = new Subject<SsvViewportMatcherContext>();
 	sizeInfo: ViewportSizeTypeInfo | undefined;
 
 	constructor(
 		private viewport: ViewportService,
 		private renderer: Renderer2,
-		private element: ElementRef,
 		private _viewContainer: ViewContainerRef,
-		templateRef: TemplateRef<NgIfContext>
+		templateRef: TemplateRef<SsvViewportMatcherContext>,
 	) {
 		this._thenTemplateRef = templateRef;
 	}
 
 	ngOnInit() {
-		console.warn("ssvViewportMatcher init");
+		console.log("ssvViewportMatcher init");
+
+		this.update$$ = this.update$
+			.pipe(
+				tap(x => console.log(">>> ssvViewportMatcher - update triggered", x)),
+				filter(() => !!this.sizeInfo),
+				tap(x => console.log(">>> ssvViewportMatcher - updating...", x)),
+				tap(() => this._updateView(this.sizeInfo!)),
+			)
+			.subscribe();
+
 		this.sizeType$$ = this.viewport.sizeType$
 			.pipe(
-				tap(x => console.info("SsvViewportMatcher - sizeType changed", x)),
-				filter(() => !!this._thenViewRef),
-				tap(x => {
-
-					if (this._thenViewRef) {
-						if (this.sizeInfo) {
-							this.renderer.removeClass(
-								this._thenViewRef.rootNodes[0],
-								`vp-size--${this.sizeInfo.name}`,
-							);
-						}
-						this.renderer.addClass(
-							// this.element.nativeElement,
-							this._thenViewRef.rootNodes[0],
-							`vp-size--${x.name}`,
-						);
-					}
-				}),
+				tap(x => console.info("ssvViewportMatcher - sizeType changed x2", x)),
 				tap(x => (this.sizeInfo = x)),
+				tap(() => this.update$.next(this._context)),
+			)
+			.subscribe();
+
+		this.cssClass$$ = this.viewport.sizeType$
+			.pipe(
+				startWith<ViewportSizeTypeInfo | undefined>(undefined),
+				filter(() => !!this._thenViewRef),
+				pairwise(),
+				tap(([prev, curr]) => {
+					const el = this._thenViewRef!.rootNodes[0];
+					if (prev) {
+						this.renderer.removeClass(el, `vp-size--${prev.name}`);
+					}
+					this.renderer.addClass(el, `vp-size--${curr!.name}`);
+				}),
 			)
 			.subscribe();
 	}
 
 	ngOnDestroy() {
+		if (this.cssClass$$) {
+			this.cssClass$$.unsubscribe();
+		}
 		if (this.sizeType$$) {
 			this.sizeType$$.unsubscribe();
+		}
+		if (this.update$$) {
+			this.update$$.unsubscribe();
 		}
 	}
 
 	@Input()
-	set ssvViewportMatcher(condition: any) {
-		this._context.$implicit = this._context.ngIf = condition;
-		this._updateView();
+	set ssvViewportMatcher(sizeType: string) {
+		console.log(">>> ssvViewportMatcher set", sizeType);
+		this._context.sizeType = sizeType;
+
+		if (this.sizeInfo) {
+			this.update$.next(this._context);
+		}
 	}
 
-	private _updateView() {
-		if (this._context.$implicit) {
+	@Input()
+	set ssvViewportMatcherExclude(sizeType: string) {
+		console.log(">>> ssvViewportMatcherExclude set", sizeType);
+		this._context.sizeTypeExclude = sizeType;
+
+		if (this.sizeInfo) {
+			this.update$.next(this._context);
+		}
+	}
+
+	private _updateView(sizeInfo: ViewportSizeTypeInfo) {
+		console.log(">>> _updateView - #0 start", sizeInfo, this._context);
+
+		if (this.shouldRender(sizeInfo, this._context)) {
 			if (!this._thenViewRef) {
 				console.log(">>> _updateView - #1 !thenViewRef");
 				this._viewContainer.clear();
 				this._elseViewRef = null;
+
 				if (this._thenTemplateRef) {
 					console.log(">>> _updateView - #2 thenTemplateRef");
 					this._thenViewRef = this._viewContainer.createEmbeddedView(
@@ -94,6 +135,7 @@ export class SsvViewportMatcher implements OnInit, OnDestroy {
 			if (!this._elseViewRef) {
 				this._viewContainer.clear();
 				this._thenViewRef = null;
+
 				if (this._elseTemplateRef) {
 					this._elseViewRef = this._viewContainer.createEmbeddedView(
 						this._elseTemplateRef,
@@ -103,4 +145,35 @@ export class SsvViewportMatcher implements OnInit, OnDestroy {
 			}
 		}
 	}
+
+	private shouldRender(
+		sizeInfo: ViewportSizeTypeInfo,
+		context: SsvViewportMatcherContext,
+	) {
+		const isIncluded = match(context.sizeType, sizeInfo.name, true);
+		const isExcluded = match(context.sizeTypeExclude, sizeInfo.name, false);
+
+		const shouldRender = isIncluded && !isExcluded;
+		console.warn(">>> shouldRender", { sizeInfo, context, shouldRender });
+		return shouldRender;
+	}
+}
+
+export class SsvViewportMatcherContext {
+	sizeType: string | string[] | null = null;
+	sizeTypeExclude: string | string[] | null = null;
+}
+
+function match(
+	sizeType: string | string[] | null,
+	currentSizeType: string,
+	defaultValue: boolean,
+) {
+	if (!sizeType) {
+		return defaultValue;
+	}
+
+	return _.isArray(sizeType)
+		? _.includes(sizeType, currentSizeType)
+		: sizeType === currentSizeType;
 }
